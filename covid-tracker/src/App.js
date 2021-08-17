2e0ae2d46e3dd408af5e59189ac9eec39e01671b
Sarthak Patipati <pusarthak@gmail.com>
2021-08-17 14:46:35 -0500

    added line

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -5,7 +5,7 @@ import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./util";
-
+import LineGraph from "./LineGraph"; 
 
 function App() {
 
@@ -102,6 +102,7 @@ console.log("Country info", countryInfo);
           <h3>Live cases by country</h3>
           <Table countries={tableData} /> 
           <h3>Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
 
       </Card>
